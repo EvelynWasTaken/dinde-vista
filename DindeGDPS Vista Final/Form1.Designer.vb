004fb1fbc5494f4a177b2c8c4ab3ebecded70a93
Evelyn <ashleyisacutiecolonthree@gmail.com>
2025-09-03 10:40:47 +0100

    1.1

--- DindeGDPS Vista Final/Form1.Designer.vb
+++ DindeGDPS Vista Final/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.inGameLogo = New System.Windows.Forms.PictureBox()
@@ -32,14 +32,23 @@ Partial Class Form1
         Me.discordLink = New System.Windows.Forms.LinkLabel()
         Me.dindeYTLink = New System.Windows.Forms.LinkLabel()
         Me.evyYTLink = New System.Windows.Forms.LinkLabel()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GDPSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SpecialToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BulgarianToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BugReportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WebsiteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         CType(Me.inGameLogo, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.gdLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'inGameLogo
         '
         Me.inGameLogo.Image = Global.DindeGDPS_Vista_Final.My.Resources.Resources.logo
-        Me.inGameLogo.Location = New System.Drawing.Point(226, 12)
+        Me.inGameLogo.Location = New System.Drawing.Point(196, 27)
         Me.inGameLogo.Name = "inGameLogo"
         Me.inGameLogo.Size = New System.Drawing.Size(417, 133)
         Me.inGameLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
@@ -48,7 +57,8 @@ Partial Class Form1
         '
         'btnInstall
         '
-        Me.btnInstall.Location = New System.Drawing.Point(186, 239)
+        Me.btnInstall.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnInstall.Location = New System.Drawing.Point(174, 236)
         Me.btnInstall.Name = "btnInstall"
         Me.btnInstall.Size = New System.Drawing.Size(113, 101)
         Me.btnInstall.TabIndex = 1
@@ -58,7 +68,7 @@ Partial Class Form1
         'btnPlay
         '
         Me.btnPlay.Enabled = False
-        Me.btnPlay.Location = New System.Drawing.Point(359, 239)
+        Me.btnPlay.Location = New System.Drawing.Point(335, 236)
         Me.btnPlay.Name = "btnPlay"
         Me.btnPlay.Size = New System.Drawing.Size(113, 101)
         Me.btnPlay.TabIndex = 2
@@ -68,16 +78,16 @@ Partial Class Form1
         'versionLabel
         '
         Me.versionLabel.AutoSize = True
-        Me.versionLabel.Location = New System.Drawing.Point(143, 421)
+        Me.versionLabel.Location = New System.Drawing.Point(110, 421)
         Me.versionLabel.Name = "versionLabel"
-        Me.versionLabel.Size = New System.Drawing.Size(385, 13)
+        Me.versionLabel.Size = New System.Drawing.Size(397, 13)
         Me.versionLabel.TabIndex = 3
-        Me.versionLabel.Text = "DindeGDPS Vista Launcher v1.0 - Launcher by Zhevelyn - DindeGDPS by Jean"
+        Me.versionLabel.Text = "DindeGDPS Legacy Launcher v1.1 - Launcher by Zhevelyn - DindeGDPS by Jean"
         '
         'newsLabel
         '
         Me.newsLabel.AutoSize = True
-        Me.newsLabel.Location = New System.Drawing.Point(183, 176)
+        Me.newsLabel.Location = New System.Drawing.Point(193, 177)
         Me.newsLabel.Name = "newsLabel"
         Me.newsLabel.Size = New System.Drawing.Size(129, 13)
         Me.newsLabel.TabIndex = 8
@@ -87,7 +97,7 @@ Partial Class Form1
         'gdLogo
         '
         Me.gdLogo.Image = Global.DindeGDPS_Vista_Final.My.Resources.Resources.dindes
-        Me.gdLogo.Location = New System.Drawing.Point(38, 12)
+        Me.gdLogo.Location = New System.Drawing.Point(26, 27)
         Me.gdLogo.Name = "gdLogo"
         Me.gdLogo.Size = New System.Drawing.Size(142, 133)
         Me.gdLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
@@ -97,7 +107,7 @@ Partial Class Form1
         'discordLink
         '
         Me.discordLink.AutoSize = True
-        Me.discordLink.Location = New System.Drawing.Point(183, 378)
+        Me.discordLink.Location = New System.Drawing.Point(157, 389)
         Me.discordLink.Name = "discordLink"
         Me.discordLink.Size = New System.Drawing.Size(43, 13)
         Me.discordLink.TabIndex = 10
@@ -107,7 +117,7 @@ Partial Class Form1
         'dindeYTLink
         '
         Me.dindeYTLink.AutoSize = True
-        Me.dindeYTLink.Location = New System.Drawing.Point(285, 378)
+        Me.dindeYTLink.Location = New System.Drawing.Point(251, 389)
         Me.dindeYTLink.Name = "dindeYTLink"
         Me.dindeYTLink.Size = New System.Drawing.Size(108, 13)
         Me.dindeYTLink.TabIndex = 11
@@ -117,18 +127,72 @@ Partial Class Form1
         'evyYTLink
         '
         Me.evyYTLink.AutoSize = True
-        Me.evyYTLink.Location = New System.Drawing.Point(422, 378)
+        Me.evyYTLink.Location = New System.Drawing.Point(385, 389)
         Me.evyYTLink.Name = "evyYTLink"
         Me.evyYTLink.Size = New System.Drawing.Size(101, 13)
         Me.evyYTLink.TabIndex = 12
         Me.evyYTLink.TabStop = True
         Me.evyYTLink.Text = "Zhevelyn's Youtube"
         '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.GDPSToolStripMenuItem, Me.SpecialToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(624, 24)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'GDPSToolStripMenuItem
+        '
+        Me.GDPSToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.GDPSToolStripMenuItem.Name = "GDPSToolStripMenuItem"
+        Me.GDPSToolStripMenuItem.Size = New System.Drawing.Size(48, 20)
+        Me.GDPSToolStripMenuItem.Text = "GDPS"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'SpecialToolStripMenuItem
+        '
+        Me.SpecialToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BulgarianToolStripMenuItem})
+        Me.SpecialToolStripMenuItem.Name = "SpecialToolStripMenuItem"
+        Me.SpecialToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.SpecialToolStripMenuItem.Text = "Special"
+        '
+        'BulgarianToolStripMenuItem
+        '
+        Me.BulgarianToolStripMenuItem.Name = "BulgarianToolStripMenuItem"
+        Me.BulgarianToolStripMenuItem.Size = New System.Drawing.Size(124, 22)
+        Me.BulgarianToolStripMenuItem.Text = "Bulgarian"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BugReportToolStripMenuItem, Me.WebsiteToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'BugReportToolStripMenuItem
+        '
+        Me.BugReportToolStripMenuItem.Name = "BugReportToolStripMenuItem"
+        Me.BugReportToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.BugReportToolStripMenuItem.Text = "Bug Report"
+        '
+        'WebsiteToolStripMenuItem
+        '
+        Me.WebsiteToolStripMenuItem.Name = "WebsiteToolStripMenuItem"
+        Me.WebsiteToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.WebsiteToolStripMenuItem.Text = "Website"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(664, 443)
+        Me.ClientSize = New System.Drawing.Size(624, 443)
         Me.Controls.Add(Me.evyYTLink)
         Me.Controls.Add(Me.dindeYTLink)
         Me.Controls.Add(Me.discordLink)
@@ -138,11 +202,18 @@ Partial Class Form1
         Me.Controls.Add(Me.btnPlay)
         Me.Controls.Add(Me.btnInstall)
         Me.Controls.Add(Me.inGameLogo)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "DindeGDPS Launcher"
         CType(Me.inGameLogo, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.gdLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -157,4 +228,12 @@ Partial Class Form1
     Friend WithEvents discordLink As LinkLabel
     Friend WithEvents dindeYTLink As LinkLabel
     Friend WithEvents evyYTLink As LinkLabel
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents GDPSToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SpecialToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BulgarianToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BugReportToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WebsiteToolStripMenuItem As ToolStripMenuItem
 End Class
--- DindeGDPS Vista Final/Form1.vb
+++ DindeGDPS Vista Final/Form1.vb
@@ -1,26 +1,26 @@
 ﻿Imports System.Net
+Imports Ionic.Zip
 Imports System.IO
 
 Public Class Form1
-    Private gameFolder As String = Path.Combine(Application.StartupPath, "gd19")
-    Private gameExe As String = Path.Combine(gameFolder, "DindeGDPS.exe")
-    Private zipUrl As String = "https://example.com/DindeGDPS.zip" ' <-- change to your file link
-    Public Property ZipFile As Object
+    Private gdpsFolder As String = Path.Combine(Application.StartupPath, "gd19")
+    Private gdpsExe As String = Path.Combine(Application.StartupPath, "gd19", "DindeGDPS.exe")
+    Private newsUrl As String = "https://isabellewastaken.141412.xyz/news.html"
+    Private zipUrl As String = "https://yourdownloadlink.com/dindegdps.zip"
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' check if game is already installed
-        If File.Exists(gameExe) Then
+        ' set install/uninstall button text
+        If Directory.Exists(gdpsFolder) AndAlso File.Exists(gdpsExe) Then
             btnInstall.Text = "Uninstall"
         Else
             btnInstall.Text = "Install"
         End If
 
-        ' fetch news
+        ' try to fetch news
         Try
             Using client As New WebClient()
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls Or SecurityProtocolType.Tls11 Or SecurityProtocolType.Tls12 Or SecurityProtocolType.Ssl3
-                Dim newsText As String = client.DownloadString("https://isabellewastaken.141412.xyz/news.html")
-                newsLabel.Text = "News: " & newsText.Trim()
+                Dim newsText As String = client.DownloadString(newsUrl).Trim()
+                newsLabel.Text = "News: " & newsText
             End Using
         Catch ex As Exception
             newsLabel.Text = "News: Failed to load"
@@ -36,65 +36,95 @@ Public Class Form1
     End Sub
 
     Private Sub InstallGame()
-        Try
-            Dim zipPath As String = Path.Combine(Application.StartupPath, "temp.zip")
+        Dim zipPath As String = Path.Combine(Application.StartupPath, "gd19.zip")
 
+        Try
+            ' download the zip
             Using client As New WebClient()
                 client.DownloadFile(zipUrl, zipPath)
             End Using
 
-            If Directory.Exists(gameFolder) Then
-                Directory.Delete(gameFolder, True)
-            End If
+            ' extract it
+            Using zip As ZipFile = ZipFile.Read(zipPath)
+                For Each entry In zip
+                    entry.Extract(gdpsFolder, ExtractExistingFileAction.OverwriteSilently)
+                Next
+            End Using
 
-            ZipFile.ExtractToDirectory(zipPath, gameFolder)
             File.Delete(zipPath)
-            ' I think ZipFile is broken, but y'know..
-            ' Too bad!
+            MessageBox.Show("Installed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
             btnInstall.Text = "Uninstall"
-            MessageBox.Show("Installation complete!", "DindeGDPS")
+
         Catch ex As Exception
-            MessageBox.Show("Install failed: " & ex.Message, "Error")
+            MessageBox.Show("Error: " & ex.Message, "Install failed", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
 
     Private Sub UninstallGame()
         Try
-            If Directory.Exists(gameFolder) Then
-                Directory.Delete(gameFolder, True)
+            If Directory.Exists(gdpsFolder) Then
+                Directory.Delete(gdpsFolder, True)
             End If
-
+            MessageBox.Show("Uninstalled successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
             btnInstall.Text = "Install"
-            MessageBox.Show("Uninstalled successfully", "DindeGDPS")
         Catch ex As Exception
-            MessageBox.Show("Uninstall failed: " & ex.Message, "Error")
+            MessageBox.Show("Error: " & ex.Message, "Uninstall failed", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
 
     Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
         Try
-            If File.Exists(gameExe) Then
-                Process.Start(gameExe)
+            If File.Exists(gdpsExe) Then
+                Process.Start(New ProcessStartInfo(gdpsExe) With {.UseShellExecute = True})
             Else
-                MessageBox.Show("Game not installed!", "Error")
+                MessageBox.Show("Game not installed!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             End If
         Catch ex As Exception
-            MessageBox.Show("Failed to start game: " & ex.Message, "Error")
+            MessageBox.Show("Error: " & ex.Message, "Failed to start", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
 
-    ' discord link
+    ' ===== links =====
     Private Sub discordLink_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles discordLink.LinkClicked
-        Process.Start("https://discord.gg/sVcFBddjfj")
+        Process.Start(New ProcessStartInfo("https://discord.gg/sVcFBddjfj") With {.UseShellExecute = True})
     End Sub
 
-    ' dinde youtube link
     Private Sub dindeYTLink_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles dindeYTLink.LinkClicked
-        Process.Start("https://www.youtube.com/@dindegdps")
+        Process.Start(New ProcessStartInfo("https://www.youtube.com/@dindegdps") With {.UseShellExecute = True})
     End Sub
 
-    ' evy youtube link
     Private Sub evyYTLink_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles evyYTLink.LinkClicked
-        Process.Start("https://www.youtube.com/@therealevelyn200")
+        Process.Start(New ProcessStartInfo("https://www.youtube.com/@therealevelyn200") With {.UseShellExecute = True})
+    End Sub
+
+    ' ===== menu strip =====
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub BulgarianToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BulgarianToolStripMenuItem.Click
+        Dim result As DialogResult = MessageBox.Show(
+            "You're probably an adult bulgarian",
+            "real",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.None
+        )
+
+        If result = DialogResult.OK Then
+            Process.Start(New ProcessStartInfo("https://www.youtube.com/@alexeez") With {.UseShellExecute = True})
+        End If
+    End Sub
+
+    Private Sub BugReportToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles BugReportToolStripMenuItem.Click
+        MessageBox.Show(
+            "If you have found any bugs, screenshot it and send it to jeantasoeur (discord) or contactus@dimisaio.be with body: ""DindeGDPS Bug!""",
+            "Bugs? Oh noes!",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information
+        )
+    End Sub
+
+    Private Sub WebsiteToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles WebsiteToolStripMenuItem.Click
+        Process.Start(New ProcessStartInfo("https://dimisaio.be/") With {.UseShellExecute = True})
     End Sub
 End Class
